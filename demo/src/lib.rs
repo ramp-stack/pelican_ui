feb1069131dd68751b004ef6dec2c927d558681c
Ella Mae <ella@orange.me>
2026-01-02 06:45:36 -0400

    wip

--- demo/src/lib.rs
+++ demo/src/lib.rs
@@ -48,9 +48,40 @@ impl DemoApp {
     }
 }
 
+#[derive(Debug, Component)]
+pub struct DemoApp2(Stack, Page);
+impl OnEvent for DemoApp2 {}
+impl AppPage for DemoApp2 {}
+impl DemoApp2 {
+    pub fn new(ctx: &mut Context) -> Self {
+        let image: Arc<RgbaImage> = Arc::new(image::open("./seagull.png").unwrap().into());
+        let img = Image{shape: ShapeType::Rectangle(0.0, (1448.0/6.0, 1904.0/6.0), 0.0), image: image.clone(), color: None};
+        let text = ExpandableText::default(ctx, "seagull.png");
+
+        let image: Arc<RgbaImage> = Arc::new(image::open("./flamingo.png").unwrap().into());
+        let img2 = Image{shape: ShapeType::Rectangle(0.0, (1448.0/6.0, 2050.0/6.0), 0.0), image: image.clone(), color: None};
+        let text2 = ExpandableText::default(ctx, "flamingo.png");
+
+        let avatar = Avatar::default(ctx);
+        let button = SecondaryButton::default(ctx);
+        let checkbox = Checkbox::default(ctx);
+        let listitem = ListItem::default(ctx);
+        let qrcode = QRCode::default(ctx);
+        let radio = RadioSelector::default(ctx);
+        let slider = Slider::default(ctx);
+        let input = TextInput::default(ctx);
+        let toggle = Toggle::default(ctx);
+
+        let content = Content::new(Offset::Start, drawables![listitem, toggle, checkbox, button, avatar, img, text]);
+        let header = Header::home(ctx, "Demo App", None);
+        Self(Stack::default(), Page::new(header, content, Some(Bumper::default(ctx))))
+    }
+}
+
 ramp::run!{|ctx: &mut Context| {
     let demo = RootInfo::icon("home", "Demo App", Box::new(DemoApp::new(ctx)));
-    Interface::new(ctx, vec![demo])
+    let demo2 = RootInfo::icon("explore", "Demo App 2", Box::new(DemoApp2::new(ctx)));
+    Interface::new(ctx, vec![demo, demo2])
 }}
 
 // for ever ycomponent we need to keep track of the state objects requeired to build it 
